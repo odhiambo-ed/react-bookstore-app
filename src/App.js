bb1dd0cd4739b2fbf4a56c9278e14fd3a43fc506
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-05-03 12:58:09 +0300

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React, { useEffect } from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { getApiBooks } from './redux/books/books';
 import Home from './Home';
 import Categories from './Categories';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getApiBooks());
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
