84ce397599ff8bd2ed7b4f95c6b0739faae2a107
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-05-04 22:33:38 +0300

    Add Categories Redux

---
+++ src/redux/Categories/Categories.js
@@ -0,0 +1,17 @@
+const CHECK_STATUS = 'bookstore/categoires/CHECK_STATUS';
+const initialData = [];
+
+export const addCategory = () => ({
+    type: CHECK_STATUS,
+});
+
+const categoriesReducer = (state = initialData, action) => {
+    switch (action.type) {
+        case CHECK_STATUS:
+            return ': Under Construction! Please Come Back Later.';
+        default:
+            return state;
+    }
+};
+
+export default categoriesReducer;
