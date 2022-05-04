56d1d6e4dccd1ff810f7288722ee830dcc89d361
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-05-04 22:26:45 +0300

    Add Books JS

---
+++ src/components/Books/Books.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import AddBook from '../AddBook/AddBook';
+import Book from '../Book/Book';
+import './Books.css';
+
+function Books() {
+    const list = useSelector((state) => state.booksReducers.list);
+    const randomNumber = Math.floor((Math.random() * 100) + 1);
+    const stringRandom = randomNumber.toString();
+    return (
+        <div>
+            <div>
+                {
+                    list.map((elem) => (
+                        <div key={elem.item_id}>
+                            <div className="card">
+                                <Book title={elem.title} author={elem.author} category={elem.category} progress={stringRandom} chapter="Chapter 17" id={elem.item_id} />
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+            <div className="hr"> </div>
+            <AddBook />
+        </div>
+    );
+}
+
+export default Books;
